358b1911387210c4ff72c2431719bed7b81fe976
ei17ringo <ei17ringo@gmail.com>
2016-04-27 15:28:39 +0800

    init commit

--- sampleTextField/ViewController.swift
+++ sampleTextField/ViewController.swift
@@ -15,6 +15,13 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    //Returnキーが押されたとき（このメソッドを作ると（Did End On Exitに紐づくメソッド）キーボードが閉じる）
+    @IBAction func tapReturn(sender: UITextField) {
+        
+        print(sender.text)
+        // TODO:ラベルを用意して、testという文字が含まれていたら「testです」とラベルに表示、それ以外は入力された字をそのまま表示しましょう
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
